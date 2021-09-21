75c260ce9a6db1b30a105b4550efc9900fb95d82
Skulllady <noorhoque45@gmail.com>
2021-09-21 14:56:26 -0400

    added event listeners

---
+++ index.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const previous = document.getElementById("previous")
+  const next = document.getElementById("next")
+
+
+  previous.addEventListener("click", () => { })
+  next.addEventListener("click", () => { })
+})

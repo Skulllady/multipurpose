d47e8766076259df52c58aaba80e8a84237196a7
Skulllady <noorhoque45@gmail.com>
2021-09-22 12:17:34 -0400

    replicated Mark's vision

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
   const previous = document.getElementById("previous")
   const next = document.getElementById("next")
+  const nav = document.querySelectorAll(".nav-items")
   const appName = document.getElementById("location")
+  const body = document.getElementById("container")
   const appNames = ['app', 'blog', 'shop', 'note', 'game', 'grave']
 
   let currentLocOfAppInCarousel = 0;
   appName.innerHTML = `${appNames[currentLocOfAppInCarousel]}`
+  let turnDark = () => {
+    if (appNames[currentLocOfAppInCarousel] === "grave") {
+      body.style.backgroundColor = "rgb(0, 0, 0)";
+      body.style.color = "rgb(117, 117, 117)";
+      previous.style.color = "rgb(117, 117, 117)";
+      next.style.color = "rgb(117, 117, 117)";
+      appName.style.color = "rgb(156, 39, 176)";
+      nav.style.color = "rgb(117, 117, 117)";
+    }
+    else {
+      body.style.backgroundColor = "white";
+      body.style.color = "black";
+      previous.style.color = "black";
+      next.style.color = "black";
+      appName.style.color = "rgb(46, 217, 235)";
+      nav.style.color = "black";
+    }
+  }
 
   //When the user clicks previous in the carousel
   previous.addEventListener("click", (e) => {
     if (currentLocOfAppInCarousel === 0) {
       currentLocOfAppInCarousel = appNames.length - 1
+
     } else {
       currentLocOfAppInCarousel--
     }
     appName.innerHTML = `${appNames[currentLocOfAppInCarousel]}`
+    turnDark();
   })
 
 
@@ -26,5 +48,6 @@ document.addEventListener('DOMContentLoaded', () => {
       currentLocOfAppInCarousel++
     }
     appName.innerHTML = `${appNames[currentLocOfAppInCarousel]}`
+    turnDark();
   })
 })

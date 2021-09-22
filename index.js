62da1695561ffe366e0d13a5c2256025d9a8a785
Skulllady <noorhoque45@gmail.com>
2021-09-22 10:54:11 -0400

    next carrot working

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
   const previous = document.getElementById("previous")
   const next = document.getElementById("next")
+  const appName = document.getElementById("location")
+  const appNames = ['app', 'blog', 'shop', 'note', 'game', 'grave']
+
+  let currentLocOfAppInCarousel = 0;
+  appName.innerHTML = `tiny ${appNames[currentLocOfAppInCarousel]}`
+
+  //When the user clicks previous in the carousel
+  previous.addEventListener("click", () => {
+
+  })
 
 
-  previous.addEventListener("click", () => { })
-  next.addEventListener("click", () => { })
+  //When the user clicks next in the carousel
+  next.addEventListener("click", (e) => {
+    if (currentLocOfAppInCarousel === appNames.length - 1) {
+      currentLocOfAppInCarousel = 0
+    } else {
+      currentLocOfAppInCarousel++
+    }
+    appName.innerHTML = `tiny ${appNames[currentLocOfAppInCarousel]}`
+  })
 })
